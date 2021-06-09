23e5669fc1747e56301f122820ccc40f9dfcc391
Em40 <tananeev61@gmail.com>
2021-06-09 21:30:46 +0300

    Customizing some stuff

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BookStoreApplication.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BookStoreApplication.Areas.Identity.IdentityHostingStartup))]
+namespace BookStoreApplication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
